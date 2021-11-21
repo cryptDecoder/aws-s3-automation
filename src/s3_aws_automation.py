4d3614e4b73fccd457e00c9fbaf9f4ea38c429cd
darkhost <cryptDecoder@gmail.com>
2021-11-21 18:46:33 +0530

    bucket creation method done

--- src/s3_aws_automation.py
+++ src/s3_aws_automation.py
@@ -6,38 +6,51 @@ import argparse
 import logging
 from colorlog import ColoredFormatter
 from minio import Minio
-from minio import Minio
-from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
-                         BucketAlreadyExists)
+from minio.error import S3Error
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 
 
 class S3_Automation(object):
-    def logger(self):
-        log = logging.getLogger(__name__)
-        ch = logging.StreamHandler()
-        formatter = ColoredFormatter(
-            '%(log_color)s %(process)d-%(levelname)s-%(message)s', '%c')
-        ch.setFormatter(formatter)
-        log.addHandler(ch)
-        log.setLevel(logging.DEBUG)
-        log.propagate = False
-        return log
+    load_dotenv()
+
+    log = logging.getLogger(__name__)
+    ch = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        '%(log_color)s %(process)d-%(levelname)s-%(message)s', '%c')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    log.setLevel(logging.DEBUG)
+    log.propagate = False
 
     def initMinio(self):
         # initialize miniClient with an endpoint and access/secret keys.
-        log = self.logger()
-        log.info("[+] INITIALIZE THE MINIO CLIENT")
+        self.log.info("[+] INITIALIZE THE MINIO CLIENT")
         minioClient = Minio(
             'play.min.io:9000',
-            access_key='Q3AM3UQ867SPQQA43P2F',
-            secret_key='zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+            access_key=os.getenv("ACCESS_KEY"),
+            secret_key=os.getenv("SECRET_KEY"),
             secure=True
         )
+        return minioClient
 
-        pass
+    def s3BucketCreation(self, s3BucketName):
+        minioClient = self.initMinio()
+        try:
+            self.log.info("[+] creating S3 bucket . . .")
+            minioClient.make_bucket(s3BucketName, location=os.getenv("AWS_REGION "))
+            self.log.debug("[+] successfully created bucket.")
+
+        except S3Error as exc:
+            self.log.error(f"[-] exception caused at\n"
+                           f"{exc}")
 
 
 s3Object = S3_Automation()
 if __name__ == '__main__':
-    s3Object.initMinio()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--s3bucket", type=str, required=True, help="please enter s3 bucket name")
+    args = parser.parse_args()
+    s3Object.s3BucketCreation(args.s3bucket)
     pass
